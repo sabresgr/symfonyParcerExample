ca2e4830b2ce7253693be39a9a082f829cd1f263
mnv <mnv@cert.by>
2019-10-12 23:03:42 +0300

    ParcerFile v1.0

--- src/Command/ParserCommand.php
+++ src/Command/ParserCommand.php
@@ -8,6 +8,8 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use App\Controller\ParcerController;
+
 
 class ParserCommand extends Command
 {
@@ -17,7 +19,7 @@ class ParserCommand extends Command
     {
         $this
             ->setDescription('Test parser')
-            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
+            ->addArgument('filename', InputArgument::OPTIONAL, 'path to csv file')
             ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
         ;
     }
@@ -25,16 +27,37 @@ class ParserCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $io = new SymfonyStyle($input, $output);
-        $arg1 = $input->getArgument('arg1');
+        $fileName = $input->getArgument('filename');
 
-        if ($arg1) {
-            $io->note(sprintf('You passed an argument: %s', $arg1));
-        }
+        self::fileNameCheck($fileName,$io);
+        print_r(ParcerController::processFile($fileName));
+
+
+
+
+
+/*
 
         if ($input->getOption('option1')) {
+            $io->comment("option");
             // ...
         }
 
         $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+*/  }
+
+
+    protected static function fileNameCheck($fileName,SymfonyStyle &$io)
+    {
+
+        if (!$fileName) {
+            $io->error('Enter input file path');
+            exit(-1);
+        }
+        if(!file_exists($fileName)) {
+            $io->error('File not exists');
+            exit(-2);
+        }
+
     }
 }
---
+++ src/Controller/ParcerController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\Encoder\CsvEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Validator\Constraints;
+use Symfony\Component\Validator\Validation;
+
+class Product
+{
+    public $code;
+    public $name;
+    public $description;
+    public $stock;
+    public $cost;
+    public $discontinued;
+}
+class ParcerController extends AbstractController
+{
+
+    public static function processFile($fileName)
+    {
+        $data=self::readFile($fileName);
+        $arrResult=array("valid"=>array(),"invalid"=>array());
+        foreach ($data as $item) {
+            $errors=self::validateProduct($item);
+            if(!count($errors))
+                $arrResult['valid'][]=$item;
+            else {
+                $arrResult['invalid'][] = $item;
+                $lastItem=count($arrResult['invalid'])-1;
+                //echo $lastItem;
+                //print_r($arrResult);
+                foreach($errors as $error)
+                    $arrResult['invalid'][$lastItem]['errors'][$error->getPropertyPath()]=$error->getMessage();
+
+            }
+        }
+        return $arrResult;
+    }
+    protected static function readFile($fileName)
+    {
+
+        $serializer = new Serializer([new ObjectNormalizer()], [new CsvEncoder()]);
+        $data = $serializer->decode(file_get_contents($fileName), 'csv');
+        return $data;
+    }
+    protected static function validateProduct($data)
+    {
+        $validator = Validation::createValidator();
+        $constraint = new Constraints\Collection(array(
+
+            'Product Code' => new Constraints\Length([
+                'min' => 1,
+                'max' => 10,
+                'minMessage' => ' must be at least {{ limit }} characters long',
+                'maxMessage' => ' cannot be longer than {{ limit }} characters',
+            ]),
+            'Product Name' => new Constraints\Length([
+                'min' => 1,
+                'max' => 50,
+                'minMessage' => ' must be at least {{ limit }} characters long',
+                'maxMessage' => ' cannot be longer than {{ limit }} characters',
+            ]),
+            'Product Description' => new Constraints\Length([
+                'min' => 1,
+                'max' => 255,
+                'minMessage' => ' must be at least {{ limit }} characters long',
+                'maxMessage' => ' cannot be longer than {{ limit }} characters',
+            ]),
+            'Stock' =>[
+                new Constraints\Type([
+                'type' => 'numeric',
+                'message' => 'The value {{ value }} is not a valid {{ type }}.',
+                ]),
+                new Constraints\GreaterThanOrEqual(['value'=>10])
+            ],
+            'Cost in GBP' => [
+                new Constraints\Type([
+                'type' => 'numeric',
+                'message' => 'The value {{ value }} is not a valid {{ type }}.',
+                ]),
+                new Constraints\LessThanOrEqual(['value'=>1000]),
+                new Constraints\GreaterThanOrEqual(['value'=>5])
+            ],
+            'Discontinued' => new Constraints\Length([
+                'min' => 0,
+                'max' => 3,
+                'minMessage' => ' must be at least {{ limit }} characters long, you have {{ value }}',
+                'maxMessage' => ' cannot be longer than {{ limit }} characters, you have {{ value }}',
+            ]),
+
+        ));
+        $violations = $validator->validate($data, $constraint);
+        return $violations;
+    }
+
+
+
+
+
+    /**
+     * @Route("/parcer", name="parcer")
+     */
+    public function index()
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/ParcerController.php',
+        ]);
+    }
+}
