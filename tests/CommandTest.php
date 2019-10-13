253edfeaecbcc16c8828146a34604376f7bddb56
mnv <mnv@cert.by>
2019-10-13 23:53:39 +0300

    Tests

--- tests/CommandTest.php
+++ tests/CommandTest.php
@@ -1,13 +1,26 @@
 <?php
 
 namespace App\Tests;
+//include_once(__DIR__."/../config/constants.php");
 
+use App\Command\ParserCommand;
+use App\Controller\ParcerController;
 use PHPUnit\Framework\TestCase;
 
 class CommandTest extends TestCase
 {
-    public function testSomething()
+    public function testCommand()
     {
-        $this->assertTrue(true);
+
+        $dir=__DIR__;
+        $output="";
+        exec("/usr/bin/php $dir/../bin/console app:parser $dir/TestFile --test ",$output,$result);
+
+        $this->assertEquals(" Number of records: 1",$output[1]);
+        $this->assertEquals("        Inserts:0  Updates: 0",$output[4]);
+        $this->assertEquals(" [OK] Number of successful: 1",$output[3]);
+
     }
+
+
 }
